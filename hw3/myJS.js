0141aedbe1336598abd3ac33982dad97af98b2d3
Aditi Anand <anandadi@umich.edu>
2021-09-26 16:49:35 -0400

    HW3 initial commit

---
+++ hw3/myJS.js
@@ -0,0 +1,54 @@
+/*
+* Excercise 1
+*
+*/
+
+const origColor = document.getElementById("color-name").textContent;
+
+/*
+* Then write a function that changes the text and the color inside the div
+*
+*/
+
+function changeColor(){
+    
+    const colorBlock = document.getElementById("color-block");
+    const colorName = document.getElementById("color-name");
+    let textColor = colorName.textContent;
+
+    // condition that determines what color it should be changed to
+    if(textColor != "#FFFF00"){
+        colorBlock.style.backgroundColor = "#FFFF00";
+        colorName.textContent = "#FFFF00";
+    }else{
+        colorBlock.style.backgroundColor = origColor;
+        colorName.textContent = origColor;
+    }
+    
+}
+
+
+/*
+* For excercise 2, you need to write an event handler for the button id "convertbtn"
+* on mouse click. For best practice use addEventListener.
+*
+*/
+convertbtn.addEventListener('click', () => {
+    let farenTemp = document.getElementById("f-input").value;
+    convertTemp(farenTemp);
+});
+
+/*
+* Then write a function that calculates Fahrenheit to Celsius and display it on the webpage
+*
+*/
+
+function convertTemp(fTemp){
+    //Calculate the temperature here
+    let cTemp = Math.round((fTemp-32) / 1.8);
+    //Send the calculated temperature to HTML
+    const cOutSpan = document.getElementById("c-output");
+    cOutSpan.textContent = cTemp;
+}
+
+
